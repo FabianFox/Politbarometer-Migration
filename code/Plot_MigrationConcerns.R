6f8a3d22445563bcbce5733c6e737384b7ec2813
FabianFox <fabianguelzau@web.de>
2023-05-15 17:26:26 +0200

    update and svr-style

--- code/Plot_MigrationConcerns.R
+++ code/Plot_MigrationConcerns.R
@@ -1,75 +1,106 @@
 # Load/install packages
 if (!require("xfun")) install.packages("xfun")
-pkg_attach2("tidyverse", "rio", "fst", "cowplot", "zoo")
+pkg_attach2("tidyverse", "rio", "hrbrthemes", "lubridate", "sjlabelled")
 
 # Load data
-barometer.orig <- import("./data/ZA2391_v12-0-0.dta")
-
-# Serialize file for faster loading
-# write.fst(barometer.df, "./data/barometer.fst")
-barometer.df <- read.fst("./data/barometer.fst")
-
-# Labels
-label_v33 <- tibble(
-  value = attributes(barometer.orig$v33)$labels,
-  label_v33 = names(value))
-
-label_v34 <- tibble(
-  value = attributes(barometer.orig$v34)$labels,
-  label_v34 = names(value))
+barometer.orig <- import("./data/ZA2391_v14-0-0.dta")
 
 # Wrangle
-barometer.df <- barometer.df %>%
-  select(month = v3, year = v4, erstnennung = v33, zweitnennung = v34, 
+barometer.df <- barometer.orig %>%
+  janitor::clean_names() %>%
+  mutate(across(c(v33, v34), ~as_character(.x), .names = "{.col}_chr")) %>%
+  select(month = v3, year = v4, erstnennung = v33_chr, zweitnennung = v34_chr, 
          repweight = 78, gesweight = 81)
 
 # Add labels
 barometer.df <- barometer.df %>%
-  left_join(y = label_v33, by = c("erstnennung" = "value")) %>%
-  left_join(y = label_v34, by = c("zweitnennung" = "value")) %>%
-  mutate(across(c(label_v33, label_v34), ~replace(.x, .x %in% c("nicht erhoben", "KA", "weiß nicht"), NA_character_)),
-         across(c(label_v33, label_v34), ~if_else(str_detect(.x, "Asyl|Ausländer"), c("Migration"), .x)),
-         migration = case_when(label_v33 == "Migration" | label_v34 == "Migration" ~1,
-                               TRUE ~ 0)) %>%
+  mutate(across(c(erstnennung, zweitnennung), ~replace(.x, .x %in% 
+                                                         c("nicht erhoben", "KA", "weiß nicht"), 
+                                                       NA_character_)),
+         across(c(erstnennung, zweitnennung), ~if_else(str_detect(.x, "Asyl|Ausländer"), 
+                                                  c("Migration"), 
+                                                  .x)),
+         migration = case_when(
+           erstnennung == "Migration" | zweitnennung == "Migration" ~ 1, TRUE ~ 0),
+         arbeitslosigkeit = case_when(
+           erstnennung == "Arbeitslosigkeit" | zweitnennung == "Arbeitslosigkeit" ~ 1, TRUE ~ 0),
+         wirtschaftslage = case_when(
+           erstnennung == "Wirtschaftslage" | zweitnennung == "Wirtschaftslage" ~ 1, TRUE ~ 0)) %>%
   group_by(month, year) %>%
-  summarise(pct = weighted.mean(migration, w = repweight)) %>%
+  summarise(across(c(migration, arbeitslosigkeit, wirtschaftslage), 
+                   ~weighted.mean(.x, w = repweight) * 100,
+                   .names = "{.col}")) %>%
   ungroup() %>%
-  mutate(date = as.yearmon(str_c(month, "/", year), "%m/%Y"))
+  mutate(date = parse_date_time(str_c(month, "/", year), "m/Y")) %>%
+  pivot_longer(cols = c(migration, arbeitslosigkeit, wirtschaftslage), names_to = "concern", 
+               values_to = "pct") %>%
+  select(-c(month, year))
+
+# Add data for 2022f. (Part I)
+barometer_new.df <- import("https://www.forschungsgruppe.de/Umfragen/Politbarometer/Langzeitentwicklung_-_Themen_im_Ueberblick/Politik_II/9_Probleme_1_1.xlsx",
+                           skip = 7) %>%
+  tibble() %>%
+  rename(date = 1) %>%
+  janitor::clean_names() %>%
+  .[-1,] %>%
+  pivot_longer(cols = -1, names_to = "concern", values_to = "pct") %>%
+  mutate(concern = if_else(concern == "auslander_integration_fluchtlinge", "migration", concern)) %>%
+  filter(concern %in% c("migration"), year(date) > 2021)
+
+# Add data for 2022f. (Part II)
+barometer_new1.df <- import("https://www.forschungsgruppe.de/Umfragen/Politbarometer/Langzeitentwicklung_-_Themen_im_Ueberblick/Politik_II/10_Probleme_2_1.xlsx",
+                            skip = 7) %>%
+  rename(date = 1) %>%
+  janitor::clean_names() %>%
+  .[-1,] %>%
+  pivot_longer(cols = -1, names_to = "concern", values_to = "pct") %>%
+  filter(concern %in% c("wirtschaftslage", "arbeitslosigkeit"), year(date) > 2021)
+
+# Merge
+barometer.df <- barometer.df %>%
+  bind_rows(list(barometer_new.df, barometer_new1.df))
+  
 
 # Politbarometer: Migration
-polit.fig <- ggplot(barometer.df, aes(x = date, y = pct)) +
-  geom_line() +
-  annotate("rect", 
-           xmin = as.yearmon("02/1996", "%m/%Y"),
-           xmax = as.yearmon("07/1996", "%m/%Y"),
-           ymax = Inf, ymin = 0, alpha = 0.3) +
-  annotate("rect", 
-           xmin = as.yearmon("03/2006", "%m/%Y"),
-           xmax = as.yearmon("08/2006", "%m/%Y"),
-           ymax = Inf, ymin = 0, alpha = 0.3) +
-  annotate("rect", 
-           xmin = as.yearmon("04/2016", "%m/%Y"),
-           xmax = as.yearmon("09/2016", "%m/%Y"),
-           ymax = Inf, ymin = 0, alpha = 0.3) +
-  annotate("text", x = as.yearmon("06/2001", "%m/%Y"), y = 1, 
-           label = "Erhebungszeitraum:\nALLBUS") +
-  annotate("segment",
-           x = as.yearmon("07/1996", "%m/%Y"), 
-           xend = as.yearmon("06/2001", "%m/%Y"),
-           y = .85, yend = .94) +
-  annotate("segment",
-           x = as.yearmon("03/2006", "%m/%Y"), 
-           xend = as.yearmon("06/2001", "%m/%Y"),
-           y = .85, yend = .94) + 
-  annotate("segment",
-           x = as.yearmon("04/2016", "%m/%Y"), 
-           xend = as.yearmon("06/2001", "%m/%Y"),
-           y = .9, yend = .94) + 
-  scale_y_continuous(labels = scales::percent) +
-  labs(x = "", y = "", title = "Wichtige Probleme in Deutschland", 
-       subtitle = 'Nennung von "Ausländer" und "Asylanten, Asyl"\n(max. zwei Nennungen)',
-       caption = 'Daten: Forschungsgruppe Wahlen "Politbarometer", gewichtet') +
-  theme_minimal_grid()
+polit.fig <- barometer.df %>%
+  filter(year(date) >= 2000) %>%
+  mutate(concern = factor(concern, 
+                          levels = c("migration", 
+                                     "arbeitslosigkeit", 
+                                     "wirtschaftslage"),
+                          labels = c("Migration", 
+                                     "Arbeitslosigkeit",
+                                     "Wirtschaftslage")),
+         date = ymd(date)) %>%
+  ggplot(aes(x = date, y = pct, colour = concern)) +
+  geom_line(size = 1.5) +
+  scale_x_date(breaks = seq.Date(as.Date("1990/01/01"), as.Date("2023/05/31"), "5 years"), date_labels = "%Y") +
+  scale_y_continuous(labels = function(x) str_c(x, "%")) +
+  scale_colour_manual(values = c("Migration" = "#0098D4",
+                                 "Arbeitslosigkeit" = "#003C76",
+                                 "Wirtschaftslage" = "#7E8015")) +
+  labs(x = "", y = "", colour = "",
+       title = "<span style = 'font-size:18pt; font-family:Tahoma;'>Wichtige Probleme in Deutschland</span><br>
+       Relativer Anteil von <span style = 'color:#0098D4;'>Migration</span>,
+       <span style = 'color:#003C76;'>Arbeitslosigkeit</span> und
+       <span style = 'color:#7E8015;'>Wirtschaftslage</span> unter allen Nennungen",
+       subtitle = 'Befragte können maximal zwei Probleme nennen.',
+       caption = 'Quelle: Forschungsgruppe Wahlen "Politbarometer"; gewichtete Daten\nAuswertung und Darstellung: Wissenschaftlicher Stab des SVR') +
+  theme_ipsum(ticks = TRUE, strip_text_size = 14, subtitle_size = 16,
+              caption_size = 14, caption_face = "plain", grid = "y",
+              axis_title_size = 16, base_family = "Tahoma", base_size = 18) +
+  theme(legend.position = "bottom", 
+        legend.text = element_text(size = 18), 
+        legend.justification = "left",
+        plot.caption = element_text(hjust = 0),
+        text = element_text(family = "Tahoma", colour = "black"),
+        axis.text = element_text(colour = "black"),
+        plot.title = ggtext::element_markdown(size = 18, lineheight = 1.5),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.major.x = element_blank(),
+        axis.line = element_line(colour = "black"),
+        axis.ticks.x = element_line(colour = "black", size = 0.5),
+        axis.ticks.y = element_line(colour = "black", size = 0.5))
 
 # Export
 ggsave("./figures/Politbarometer.png", polit.fig, height = 13, width = 20, units = "cm")
